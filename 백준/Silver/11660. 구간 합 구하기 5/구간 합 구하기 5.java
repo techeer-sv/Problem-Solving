a422a9f25664657c03aa3f8969891b0e0822f389
Soobin Jung <76704035+SoobinJung1013@users.noreply.github.com>
2022-07-24 23:36:06 -0700

    Update 구간 합 구하기 5.java

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,38 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        int A[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st =
+            new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+        int quizNum = Integer.parseInt(st.nextToken());
+
+        // n x n 배열 초기화
+        int arr[][] = new int[size + 1][size + 1];
+        for (int i = 1; i <= size; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= size; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        int D[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
+
+        // 2차원 구간합 배열 초기화
+        int sumArr[][] = new int[size + 1][size + 1];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j]
+                                    - sumArr[i-1][j-1] + arr[i][j];
             }
         }
-        
-        for (int i = 0; i < M; i++) {
+
+        // 2차원 구간합 구하기
+        for (int i = 0; i < quizNum; i++) {
             st = new StringTokenizer(br.readLine());
+
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            
-            int result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
+
+            int result = sumArr[x2][y2] - sumArr[x1-1][y2]
+                            - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
             System.out.println(result);
         }
     }
+
 }
