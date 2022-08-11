ce2c08aff75e0341399a1fa1c651bcc8723cec5d
Somi <rlathal009@gmail.com>
2022-08-12 00:15:05 +0900

    [Gold V] Title: 암호 만들기, Time: 140 ms, Memory: 14228 KB -BaekjoonHub

---
+++ Somi/백준/Gold/1759.암호 만들기/암호 만들기.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int L, C;
+    public static char[] list;
+    public static char[] code;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        list = new char[C];
+        code = new char[L];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<C; i++){
+            list[i] = st.nextToken().charAt(0);
+        }
+
+        Arrays.sort(list);
+        makeCode(0,0);
+    }
+
+    public static void makeCode(int x, int idx){
+        if(idx == L) {
+            if(isValid()){
+                System.out.println(code);
+            }
+            return;
+        }
+
+        for(int i=x; i<C; i++){
+            code[idx] = list[i];
+            makeCode(i+1, idx+1);
+        }
+    }
+
+    public static boolean isValid(){
+        int mo = 0;
+        int ja = 0;
+
+        for(char x : code){
+            if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+                mo++;
+            } else{
+                ja++;
+            }
+        }
+
+        if(mo>=1 && ja>=2) return true;
+        else return false;
+    }
+    
+}
